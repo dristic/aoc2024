b4b42201091b804602bb35e2263654323f9ddfaf
Dan Ristic <dristic101@gmail.com>
2024-12-02 22:34:33 -0800

    Day 2

--- src/d2.rs
+++ src/d2.rs
@@ -1,16 +1,79 @@
 use std::{error::Error, fs};
 
 pub fn run() -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string("input/d2s.txt")?;
+    let content = fs::read_to_string("input/d2.txt")?;
 
-    let mut v1 = Vec::new();
-    let mut v2 = Vec::new();
+    let mut reports = Vec::new();
 
     for line in content.lines() {
-        let parts: Vec<_> = line.split("   ").collect();
-        v1.push(parts[0].parse::<i32>()?);
-        v2.push(parts[1].parse::<i32>()?);
+        let parts = line
+            .split(" ")
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect::<Vec<_>>();
+        reports.push(parts);
     }
 
+    let safe = reports
+        .iter()
+        .map(|v| is_safe(v, None))
+        .filter(|r| *r)
+        .count();
+
+    println!("Ans1 {safe}");
+
+    let safe = reports
+        .iter()
+        .map(|v| is_safe_dampener(v))
+        .filter(|r| *r)
+        .count();
+
+    println!("Ans2 {safe}");
+
     Ok(())
+}
+
+fn is_safe(levels: &[i32], skip: Option<usize>) -> bool {
+    let mut last_delta = None;
+    let mut last = None;
+    let skip = skip.unwrap_or(usize::MAX);
+
+    for (i, n) in levels.iter().enumerate() {
+        if i == skip {
+            continue;
+        }
+
+        if let Some(last) = last {
+            let delta: i32 = n - last;
+
+            if delta.abs() > 3 || delta.abs() < 1 {
+                return false;
+            }
+
+            if let Some(last_delta) = last_delta {
+                if delta ^ last_delta < 0 {
+                    return false;
+                }
+            }
+
+            last_delta = Some(delta);
+        }
+
+        last = Some(n);
+    }
+
+    true
+}
+
+fn is_safe_dampener(levels: &[i32]) -> bool {
+    if !is_safe(levels, None) {
+        let result = levels
+            .iter()
+            .enumerate()
+            .map(|(i, _)| is_safe(levels, Some(i)))
+            .any(|r| r);
+
+        return result;
+    }
+
+    true
 }
