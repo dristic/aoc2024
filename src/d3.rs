99fad4672f3f95ddc92e582c17b1cda4d6568268
Dan Ristic <dristic101@gmail.com>
2024-12-03 21:38:08 -0800

    Day 3

--- src/d3.rs
+++ src/d3.rs
@@ -1,42 +1,82 @@
 use std::{error::Error, fs};
 
 use nom::{
+    branch::alt,
     bytes::complete::{tag, take_while},
     character::{
         complete::{anychar, char},
         is_digit,
     },
+    combinator::value,
     multi::{many0, many_till},
     sequence::{delimited, tuple},
     IResult,
 };
 
+#[derive(Clone)]
+enum Value {
+    Mul((i32, i32)),
+    Do,
+    Dont,
+}
+
 pub fn run() -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string("input/d3.txt")?;
 
-    let (_, pairs) = parse(&content).unwrap();
+    let (_, values) = parse_muls(&content).unwrap();
 
-    let ans = pairs
+    let ans = values
         .iter()
-        .map(|(s1, _, s2)| {
-            let n1 = s1.parse::<i32>().unwrap();
-            let n2 = s2.parse::<i32>().unwrap();
-            n1 * n2
+        .filter_map(|v| {
+            if let Value::Mul((n1, n2)) = v {
+                Some(n1 * n2)
+            } else {
+                None
+            }
         })
         .sum::<i32>();
 
     println!("Part1 {ans}");
 
+    let (_, values) = parse_muls(&content).unwrap();
+
+    let mut enabled = true;
+    let mut ans = 0;
+
+    for val in values {
+        match val {
+            Value::Mul((n1, n2)) => if enabled { ans += n1 * n2; },
+            Value::Do => enabled = true,
+            Value::Dont => enabled = false,
+        }
+    }
+
+    println!("Part2 {ans}");
+
     Ok(())
 }
 
-fn parse(input: &str) -> IResult<&str, Vec<(&str, char, &str)>> {
+fn parse_muls(input: &str) -> IResult<&str, Vec<Value>> {
     many0(next_mul)(input)
 }
 
-fn next_mul(input: &str) -> IResult<&str, (&str, char, &str)> {
-    let (input, (_, found)) = many_till(anychar, mul)(input)?;
+fn next_mul(input: &str) -> IResult<&str, Value> {
+    let (input, (_, found)) = many_till(anychar, val)(input)?;
     Ok((input, found))
+}
+
+fn val(input: &str) -> IResult<&str, Value> {
+    if let Ok((input, (n1, _, n2))) = mul(input) {
+        Ok((
+            input,
+            Value::Mul((n1.parse::<i32>().unwrap(), n2.parse::<i32>().unwrap())),
+        ))
+    } else {
+        alt((
+            value(Value::Do, tag("do()")),
+            value(Value::Dont, tag("don't()")),
+        ))(input)
+    }
 }
 
 fn mul(input: &str) -> IResult<&str, (&str, char, &str)> {
